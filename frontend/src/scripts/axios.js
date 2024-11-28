c5b2763d7ec2ea4a27b519474c8e01c89153ae0c
jmugisha1 <144003057+jmugisha1@users.noreply.github.com>
2024-11-28 13:17:33 +0200

    Update axios.js

--- frontend/src/scripts/axios.js
+++ frontend/src/scripts/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://taakarwanda.pythonanywhere.com/",
+  baseURL: "https://taakarwanda.pythonanywhere.com/",
   headers: {
     "Content-Type": "application/json",
   },
