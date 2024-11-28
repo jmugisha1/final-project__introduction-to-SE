5eadb4c472c0f10e70b32490ace99224220e47b3
jmugisha1 <144003057+jmugisha1@users.noreply.github.com>
2024-11-28 13:21:46 +0200

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,8 +14,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://taakarwanda.vercel.app/",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
